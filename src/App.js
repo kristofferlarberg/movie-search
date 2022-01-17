7390be5f15e758fddd98b98b6e099b2fc35fed99
kristofferlarberg <mail@kristofferlarberg.se>
2022-01-17 15:08:44 +0100

    Sort and render data based on categories

--- src/App.js
+++ src/App.js
@@ -4,20 +4,54 @@ import MenuButton from "./menuButton.svg"
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [searchString, setSearchString] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState();
+
+  const path = "https://image.tmdb.org/t/p/w500/";
+  let categories;
 
   function toggleMenu() {
     setIsOpen(!isOpen);
   }
 
+  useEffect(() => {
+    //Gets the genre specifications
+    async function getCategories() {
+      const response = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=27cfec6c9eb8080cb7d8025ba420e2d7&language=en-US")
+      const data = await response.json();
+      categories = {
+        action: data.genres[0].id,
+        drama: data.genres[6].id,
+        comedy: data.genres[3].id
+      };
+      console.log(categories)
+    }
+    getCategories();
+  }, [])
+
+  //Returns the movies based on search query
   async function handleSubmit(event) {
     event.preventDefault();
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=27cfec6c9eb8080cb7d8025ba420e2d7&language=en-US&query=${searchString}&page=1&include_adult=false`)
     const data = await response.json();
-    setResults(data.results)
+    const drama = [];
+    const action = [];
+    const comedy = []
+
+    console.log(data.results)
+
+    //Didn't manage to sort based on category-object fetched in useEffect, hard coded values instead.
+    data.results.forEach((result) => {
+      result.genre_ids.forEach((id) => {
+        if (id === 28) action.push(result)
+        if (id === 18) drama.push(result)
+        if (id === 35) comedy.push(result)
+      })
+    })
+    setResults({ drama, action, comedy })
   }
 
-  console.log(results);
+  console.log("results done", results)
+
 
   return (
     <div className="">
@@ -56,13 +90,42 @@ function App() {
           />
           <button>Search</button>
         </form>
-        <section>
-          <h2>Category</h2>
+        {results && <section>
+          <h2>Drama</h2>
           <div className="container">
-            <img src="img/test.png"></img>
-            <img src="img/test.png"></img>
+            {results.drama.map((result) => (
+              <>
+                {result.poster_path && <img src={path + result.poster_path}></img>}
+                {result.title && <p>{result.title}</p>}
+                {result.release_date && <p>{result.release_date.slice(0, 4)}</p>}
+              </>
+            ))}
           </div>
-        </section>
+        </section>}
+        {results && <section>
+          <h2>Action</h2>
+          <div className="container">
+            {results.action.map((result) => (
+              <>
+                {result.poster_path && <img src={path + result.poster_path}></img>}
+                {result.title && <p>{result.title}</p>}
+                {result.release_date && <p>{result.release_date.slice(0, 4)}</p>}
+              </>
+            ))}
+          </div>
+        </section>}
+        {results && <section>
+          <h2>Comedy</h2>
+          <div className="container">
+            {results.comedy.map((result) => (
+              <>
+                {result.poster_path && <img src={path + result.poster_path}></img>}
+                {result.title && <p>{result.title}</p>}
+                {result.release_date && <p>{result.release_date.slice(0, 4)}</p>}
+              </>
+            ))}
+          </div>
+        </section>}
       </main>
       <footer>
         The Movie Finder
