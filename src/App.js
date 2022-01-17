0b954c3a9e60188983f2e68422bd226f4dda288b
kristofferlarberg <mail@kristofferlarberg.se>
2022-01-17 10:51:48 +0100

    Add styling and mock image for main content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 function App() {
   return (
     <div className="">
-      <header className="">
-        <h1>Hello movie</h1>
+      <header>
+        The Movie Finder
       </header>
       <main>
+        <h1 className="hidden">Search for your movie</h1>
+        <form>
+          <label for="site-search" className="hidden">
+            Search for your movie:
+          </label>
+          <input type="search" id="site-search" name="q"
+            aria-label="Search through site content" />
+          <button>Search</button>
+        </form>
+        <section>
+          <h2>Category</h2>
+          <div className="container">
+            <img src="img/test.png"></img>
+            <img src="img/test.png"></img>
+          </div>
+        </section>
       </main>
-      <footer className="">
-        <p>Movie Search</p>
+      <footer>
+        The Movie Finder
       </footer>
     </div>
   );
